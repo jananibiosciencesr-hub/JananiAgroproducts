44529fe48dde06f27a936e74c8ea7cabbc6fa435
SaiRama-Doddi <sairama1026@gmail.com>
2026-09-17 22:12:51 +0530

    feat: add backend database schema and update timezone configuration

--- database.sql
+++ database.sql
@@ -8,7 +8,7 @@
 SET FOREIGN_KEY_CHECKS = 0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+05:30";
+SET time_zone = "+00:00";
 
 -- --------------------------------------------------------
 -- Table structure for `users`
